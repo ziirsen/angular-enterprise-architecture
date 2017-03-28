fd9017c38f787404a008866f12fd627e2768c205
Thomas Ziirsen <drextzir@dr.dk>
2017-03-28 10:58:14 +0200

    Configured Nightwatch

---
+++ AEA.EndToEndTesting/_Tests/test.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    'Can load chart page': function (browser) {
+        browser
+          .url('http://localhost:22400/')
+          .waitForElementVisible('body', 1000)
+          .waitForElementVisible('a[id=btn-chart]', 1000)
+          .click('a[id=btn-chart]')
+          .pause(1000)
+          .assert.containsText('h1', 'Charting it out')
+          .end();
+    }
+};
